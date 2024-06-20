8cf92e8e9ee7fed43e7c66713a99899f65866ad1
Dilley Andrade <dilleyandrade@gmail.com>
2024-06-20 07:24:48 -0300

    refactor: added categories types

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,22 @@
 import express from 'express'
-
 import {v4 as uuidV4} from 'uuid'
+import { Category } from '../model/Category'
+
 
 const categoriesRoutes = express.Router()
 
-const categories: any = []
+const categories: Category[] = []
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body
 
-  const category = {
+  const category = new Category()
+  
+  Object.assign(category, {
     name,
     description,
-    id: uuidV4()
-  }
+    created_at: new Date()
+  })
 
   categories.push(category)
 
