0bdd34a3d6163ff03b61be8b933468fb0cb664c7
Dilley Andrade <dilleyandrade@gmail.com>
2024-06-20 07:57:20 -0300

    feat: added repository category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,27 +1,18 @@
 import express from 'express'
-import { Category } from '../model/Category'
+import { CategoriesRepository } from '../repositories/categoriesRepository'
 
 
 const categoriesRoutes = express.Router()
 
-const categories: Category[] = []
+const categoriesRepository = new CategoriesRepository()
+
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body
 
-  const category = new Category()
-  
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date()
-  })
+  categoriesRepository.create({name, description})
 
-  categories.push(category)
-
-  return res.status(201).json([
-    categories
-  ])
+  return res.status(201).send()
 
 } )
 
